70b5b9ba76cc83abf72f31487495cb91b1afc991
pdelboca <patriciodelboca@gmail.com>
2020-01-26 11:27:06 -0300

    Basic new, update, delete

--- ckanext/gitdatahub/plugin.py
+++ ckanext/gitdatahub/plugin.py
@@ -3,6 +3,7 @@ import logging
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
 
+from ckanapi import datapackage
 from github import Github
 
 log = logging.getLogger(__name__)
@@ -14,7 +15,6 @@ class GitDataHubException(Exception):
 class GitdatahubPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
     plugins.implements(plugins.IPackageController, inherit=True)
-    plugins.implements(plugins.IResourceController, inherit=True)
 
     # IConfigurer
     def configure(self, config):
@@ -28,23 +28,37 @@ class GitdatahubPlugin(plugins.SingletonPlugin):
         toolkit.add_public_directory(config_, 'public')
         toolkit.add_resource('fanstatic', 'gitdatahub')
 
-    # IPackageController
-    # TODO: after_create is both for IPackageController and IResourceController
     def after_create(self, context, pkg_dict):
-        import ipdb; ipdb.set_trace()
-        if pkg_dict['state'] == 'active':
-            token = toolkit.config.get('ckanext.gitdatahub.access_token')
-            try:
-                g = Github(token)
-                repo = g.get_repo('pdelboca/gitdatahub-package')
-                repo.create_file(
-                    'datapackage.json',
-                    'Create datapackage.json',
-                    json.dumps(pkg_dict)
-                    )
-            except Exception as e:
-                log.exception('Cannot create datapackage.json file.')
+        token = toolkit.config.get('ckanext.gitdatahub.access_token')
+        try:
+            g = Github(token)
+            repo = g.get_repo('pdelboca/gitdatahub-package')
+            body = datapackage.dataset_to_datapackage(pkg_dict)
+            repo.create_file(
+                'datapackage.json',
+                'Create datapackage.json',
+                json.dumps(body, indent=2)
+                )
+        except Exception as e:
+            log.exception('Cannot create datapackage.json file.')
 
+    #TODO: When updating after the creation resources are added, but not when
+    # updating an already created package.
+    def after_update(self, context, pkg_dict):
+        token = toolkit.config.get('ckanext.gitdatahub.access_token')
+        try:
+            g = Github(token)
+            repo = g.get_repo('pdelboca/gitdatahub-package')
+            contents = repo.get_contents("datapackage.json")
+            body = datapackage.dataset_to_datapackage(pkg_dict)
+            repo.update_file(
+                contents.path,
+                "Update datapackage.json",
+                json.dumps(body, indent=2),
+                contents.sha
+                )
+        except Exception as e:
+            log.exception('Cannot update datapackage.json file.')
 
     def delete(self, entity):
         token = toolkit.config.get('ckanext.gitdatahub.access_token')
